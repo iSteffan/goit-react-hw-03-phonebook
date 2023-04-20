4c5f170e3325e9b06d322a1dd46e87ed8128dcda
Stepan Skichko <st.skichko@gmail.com>
2023-04-20 11:40:19 +0300

    add styles

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,11 +1,12 @@
-import { Form, Label, Btn } from './ContactForm.styled';
-import { Formik, Field } from 'formik';
+import { Form, Label, Btn, Field, ErrorMessage } from './ContactForm.styled';
+import PropTypes from 'prop-types';
+import { Formik } from 'formik';
 import * as Yup from 'yup';
 import { nanoid } from 'nanoid';
 
 const FormSchema = Yup.object().shape({
   name: Yup.string().required('Required field!'),
-  number: Yup.number().required('Required field!'),
+  number: Yup.number().positive('Must be > 0!').required('Required field!'),
 });
 
 export const ContactForm = ({ onSave }) => {
@@ -26,11 +27,13 @@ export const ContactForm = ({ onSave }) => {
           Name
           <br />
           <Field name="name" />
+          <ErrorMessage name="name" component="div" />
         </Label>
         <Label>
           Number
           <br />
           <Field type="number" name="number" />
+          <ErrorMessage name="number" component="div" />
         </Label>
         <Btn type="submit">Add contact</Btn>
       </Form>
@@ -38,6 +41,6 @@ export const ContactForm = ({ onSave }) => {
   );
 };
 
-// Notification.propTypes = {
-//     message: PropTypes.string.isRequired,
-// };
+Notification.propTypes = {
+  onSave: PropTypes.func.isRequired,
+};
